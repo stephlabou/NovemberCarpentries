96a983f0a85659110b1b81093b23f0f78acfde30
Stephanie Labou <slabou@ucsd.edu>
2025-11-05 09:48:14 -0800

    compare R and Python

--- code.R
+++ code.R
@@ -3,3 +3,6 @@
 x <- 10
 y <- x + 3
 z <- y - x
+
+#idea for analysis
+z + y + x
--- code.py
+++ code.py
@@ -4,3 +4,6 @@ y = x + 2
 #note to self be sure to update code
 
 import pandas as pd
+
+#try same analysis as in R
+x + y
